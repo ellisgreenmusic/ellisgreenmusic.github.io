2d695cc69bdefedd6c375cb13ae23c306398509b
tsgreen1213 <84605164+tsgreen1213@users.noreply.github.com>
2021-10-07 15:18:07 -0700

    Revamp v1

---
+++ scripts/menu.js
@@ -0,0 +1,12 @@
+let sidebar = document.getElementById("sidebar-mobile");
+let overlay = document.getElementById("overlay");
+function openMenu() {
+	sidebar.style.left = "0";
+	overlay.style.opacity = ".5";
+	overlay.style.pointerEvents = "auto";
+}
+function closeMenu() {
+	sidebar.style.left = "-90%";
+	overlay.style.opacity = "0";
+	overlay.style.pointerEvents = "none";
+}
