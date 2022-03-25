513df3d89e76945b51bbc37394dd7fab6f5aa8c2
ellisgreenmusic <91094240+ellisgreenmusic@users.noreply.github.com>
2022-03-25 10:54:43 -0700

    Swipy

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,7 @@
 let sidebar = document.getElementById("sidebar-mobile");
 let overlay = document.getElementById("overlay");
 function openMenu() {
+	sidebar.style.marginLeft = "0px";
 	sidebar.style.left = "0";
 	overlay.style.opacity = ".5";
 	overlay.style.pointerEvents = "auto";
@@ -10,3 +11,25 @@ function closeMenu() {
 	overlay.style.opacity = "0";
 	overlay.style.pointerEvents = "none";
 }
+//Swiping!
+let startX = 0;
+let startY = 0;
+sidebar.addEventListener("touchstart", e => {
+	startX = e.changedTouches[0].screenX;
+	startY = e.changedTouches[0].screenY;
+});
+sidebar.addEventListener("touchend", e => {	
+	let distanceX = e.changedTouches[0].screenX - startX;
+	let distanceY = e.changedTouches[0].screenY - startY;
+	if(distanceX < -10 & Math.abs(distanceX) > Math.abs(distanceY)) {
+		closeMenu();
+	} else {
+		sidebar.style.marginLeft = "0px";
+	}
+});
+sidebar.addEventListener("touchmove", e => {	
+	let distanceX = e.changedTouches[0].screenX - startX;
+	if(distanceX < 0) {
+		sidebar.style.marginLeft = distanceX + "px";
+	}
+});
