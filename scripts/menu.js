cef5be9730f74d903cd31ebaf57f41e1b9a21674
ellisgreenmusic <91094240+ellisgreenmusic@users.noreply.github.com>
2022-03-25 13:50:04 -0700

    Swipier

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,35 +1,55 @@
-let sidebar = document.getElementById("sidebar-mobile");
 let overlay = document.getElementById("overlay");
+let sidebar = document.getElementById("sidebar-mobile");
 function openMenu() {
-	sidebar.style.marginLeft = "0px";
+	overlay.classList.add("overlay-visible");
 	sidebar.style.left = "0";
-	overlay.style.opacity = ".5";
-	overlay.style.pointerEvents = "auto";
 }
 function closeMenu() {
+	overlay.classList.remove("overlay-visible");
+	sidebar.classList.remove("transitionless");
 	sidebar.style.left = "-90%";
-	overlay.style.opacity = "0";
-	overlay.style.pointerEvents = "none";
 }
 //Swiping!
-let startX = 0;
-let startY = 0;
+let maximumX;
+let maximumY;
+let previousX;
+let goingLeft;
 sidebar.addEventListener("touchstart", e => {
-	startX = e.changedTouches[0].screenX;
-	startY = e.changedTouches[0].screenY;
+	sidebar.classList.add("transitionless");
+	maximumX = e.changedTouches[0].screenX;
+	maximumY = e.changedTouches[0].screenY;
+	previousX = maximumX;
+});
+sidebar.addEventListener("touchmove", e => {
+	let currentX = e.changedTouches[0].screenX;
+	let currentY = e.changedTouches[0].screenY;
+	let distance = currentX - maximumX;
+	if(distance < 0) {
+		sidebar.style.left = distance + "px";
+	}
+	//Find direction
+	if(currentX - previousX < 0) {
+		goingLeft = true;
+	} else {
+		goingLeft = false;
+	}
+	//Update maximums
+	if(currentX > maximumX) {
+		maximumX = currentX;
+	}
+	if(currentY > maximumY) {
+		maximumY = currentY;
+	}
+	//Set previousX
+	previousX = currentX;
 });
 sidebar.addEventListener("touchend", e => {	
-	let distanceX = e.changedTouches[0].screenX - startX;
-	let distanceY = e.changedTouches[0].screenY - startY;
-	if(distanceX < -10 & Math.abs(distanceX) > Math.abs(distanceY)) {
+	let distanceX = e.changedTouches[0].screenX - maximumX;
+	let distanceY = e.changedTouches[0].screenY - maximumY;
+	if(goingLeft === true && distanceX < -10 && Math.abs(distanceX) > Math.abs(distanceY)) {
 		closeMenu();
 	} else {
-		sidebar.style.marginLeft = "0px";
-	}
-});
-sidebar.addEventListener("touchmove", e => {	
-	let distanceX = e.changedTouches[0].screenX - startX;
-	if(distanceX < 0) {
-		sidebar.style.marginLeft = distanceX + "px";
+		sidebar.classList.remove("transitionless");
+		sidebar.style.left = "0";
 	}
 });
